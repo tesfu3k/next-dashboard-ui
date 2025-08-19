3895eceda466398f6515158390c68f2e0f066796
tesfu3k <tesfaye4r@gmail.com>
2025-08-19 19:55:07 +0300

    Calander has been added in admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import EventCalander from "@/components/EventCalander";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
@@ -32,7 +33,9 @@ const AdminPage = () => {
         </div>
       </div>
       {/* Right */}
-      <div className="w-full lg:w-1/3">Right</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalander />
+      </div>
     </div>
   );
 };
