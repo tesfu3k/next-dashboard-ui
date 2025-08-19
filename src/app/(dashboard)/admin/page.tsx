879b2d4f8339e7ae468cc290f1742f49c8d4b5af
tesfu3k <tesfaye4r@gmail.com>
2025-08-19 21:12:18 +0300

    admin page design has been completed

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import Announcements from "@/components/Announcements";
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
 import EventCalander from "@/components/EventCalander";
@@ -8,7 +9,7 @@ const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* Left */}
-      <div className="w-full lg:w-2/3 flex-col gap-8">
+      <div className="w-full flex lg:w-2/3 flex-col gap-8">
         {/* User Cards */}
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="student" />
@@ -35,6 +36,7 @@ const AdminPage = () => {
       {/* Right */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
         <EventCalander />
+        <Announcements />
       </div>
     </div>
   );
--- src/components/EventCalander.tsx
+++ src/components/EventCalander.tsx
@@ -36,7 +36,7 @@ const EventCalander = () => {
   return (
     <div className="bg-white p-4 rounded-md">
       <Calendar onChange={onChange} value={value} />
-      <div className="flex item-center justify-between">
+      <div className="flex items-center justify-between">
         <h1 className="text-lg font-semibold my-4">Events</h1>
         <Image src="/moreDark.png" alt="" width={20} height={20} />
       </div>
