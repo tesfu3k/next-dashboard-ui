867a1d46d43c80b47f646737a1821427f3b7c608
tesfu3k <tesfaye4r@gmail.com>
2025-08-20 15:18:59 +0300

    parent page has been designed

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,10 +1,9 @@
 import Announcements from "@/components/Announcements";
 import BigCalendar from "@/components/BigCalendar";
-import EventCalander from "@/components/EventCalander";
 
 const TeacherPage = () => {
   return (
-    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
       {/* Left */}
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
