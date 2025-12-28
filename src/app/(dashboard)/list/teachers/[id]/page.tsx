2d014b50ffad79ff76d577a3659c4fd8ad1fba01
tesfu3k <tesfaye4r@gmail.com>
2025-12-28 16:00:22 +0300

    feat: enhance ExamListPage with dynamic data fetching and pagination; update links in SingleStudentPage and SingleTeacherPage to include class and teacher IDs

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -158,10 +158,16 @@ const SingleTeacherPage = () => {
             >
               Teacher&apos;s Lessons
             </Link>
-            <Link className="p-3 rounded-md bg-pink-50" href="/">
+            <Link
+              className="p-3 rounded-md bg-pink-50"
+              href={`/list/exams?teacherId=${"teacher2"}`}
+            >
               Teacher&apos;s Exams
             </Link>
-            <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">
+            <Link
+              className="p-3 rounded-md bg-lamaSkyLight"
+              href={`/list/assignments?teacherId=${"teacher2"}`}
+            >
               Teacher&apos;s Assignments
             </Link>
           </div>
