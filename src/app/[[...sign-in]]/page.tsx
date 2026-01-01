d1a15e4040eceeadacb5dcfb83b6ac22365fdbc2
tesfu3k <tesfaye4r@gmail.com>
2026-01-01 21:03:42 +0300

    feat: implement role-based route access control and enhance login redirection

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -4,10 +4,21 @@ import * as Clerk from "@clerk/elements/common";
 import * as SignIn from "@clerk/elements/sign-in";
 import { useUser } from "@clerk/nextjs";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const LoginPage = () => {
   const { isSignedIn, isLoaded, user } = useUser();
-  console.log(user);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const role = user?.publicMetadata.role;
+
+    if (role) {
+      router.push(`/${role}`);
+    }
+  }, [user, router]);
 
   return (
     <div className="h-screen flex justify-center items-center bg-lammSkyLight">
