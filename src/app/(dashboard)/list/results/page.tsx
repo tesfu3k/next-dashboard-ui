e6bab8726b931a6b595de68c429839d3b97dcd8a
tesfu3k <tesfaye4r@gmail.com>
2026-01-04 15:41:52 +0300

    feat: enhance role-based access and filtering in ResultListPage

--- src/app/(dashboard)/list/results/page.tsx
+++ src/app/(dashboard)/list/results/page.tsx
@@ -2,11 +2,13 @@ import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { role } from "@/lib/data";
+
 import prisma from "@/lib/prisma";
 import { ITEM_PER_PAGE } from "@/lib/settings";
-import { Prisma } from "@prisma/client";
+import { Prisma } from "@/lib/generated/prisma/client";
 import Image from "next/image";
+import { CurrentUserId, role } from "@/lib/utils";
+import { currentUser } from "@clerk/nextjs/server";
 
 type ResultList = {
   id: number;
@@ -53,11 +55,14 @@ const columns = [
     accessor: "type",
     className: "hidden md:table-cell",
   },
-
-  {
-    header: "Actions",
-    accessor: "actions",
-  },
+  ...(role === "admin" || role === "teacher"
+    ? [
+        {
+          header: "Actions",
+          accessor: "actions",
+        },
+      ]
+    : []),
 ];
 
 const renderRow = (item: ResultList) => (
@@ -78,7 +83,7 @@ const renderRow = (item: ResultList) => (
 
     <td>
       <div className="flex items-center gap-2">
-        {role === "admin" && (
+        {(role === "admin" || role === "teacher") && (
           <>
             <FormModal table="result" type="update" data={item} />
             <FormModal table="result" type="delete" id={item.id} />
@@ -121,6 +126,29 @@ const ResultListPage = async ({
         }
       }
     }
+  }
+
+  // ROLE CONDITIONS
+
+  switch (role) {
+    case "admin":
+      break;
+    case "teacher":
+      query.OR = [
+        { exam: { lesson: { teacherId: CurrentUserId! } } },
+        { assignment: { lesson: { teacherId: CurrentUserId! } } },
+      ];
+      break;
+    case "student":
+      query.studentId = CurrentUserId!;
+      break;
+    case "parent":
+      query.student = {
+        parentId: CurrentUserId!,
+      };
+      break;
+    default:
+      break;
   }
 
   const [dataRes, count] = await prisma.$transaction([
@@ -201,7 +229,9 @@ const ResultListPage = async ({
                 height={14}
               />
             </button>
-            {role === "admin" && <FormModal table="result" type="update" />}
+            {(role === "admin" || role === "teacher") && (
+              <FormModal table="result" type="update" />
+            )}
           </div>
         </div>
       </div>
