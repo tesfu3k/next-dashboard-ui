73978c72badb0377c8d9192501aff832d99997c3
tesfu3k <tesfaye4r@gmail.com>
2026-01-04 15:55:15 +0300

    feat: update import structure and enhance role-based access in SubjectListPage

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -2,12 +2,12 @@ import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { role, subjectsData } from "@/lib/data";
+
 import prisma from "@/lib/prisma";
 import { ITEM_PER_PAGE } from "@/lib/settings";
-import { Prisma, Subject, Teacher } from "@prisma/client";
+import { Prisma, Subject, Teacher } from "@/lib/generated/prisma/client";
 import Image from "next/image";
-import Link from "next/link";
+import { role } from "@/lib/utils";
 
 type SubjectList = Subject & { teachers: Teacher[] };
 
@@ -21,10 +21,14 @@ const columns = [
     accessor: "teachers",
     className: "hidden md:table-cell",
   },
-  {
-    header: "Actions",
-    accessor: "actions",
-  },
+  ...(role === "admin"
+    ? [
+        {
+          header: "Actions",
+          accessor: "actions",
+        },
+      ]
+    : []),
 ];
 
 const renderRow = (item: SubjectList) => (
